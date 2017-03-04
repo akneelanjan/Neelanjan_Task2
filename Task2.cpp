52f834f2ae0286360dcbf93514a1d421e5949c92
NEELANJAN AKULI <akneelanjan@gmail.com>
2017-03-04 23:42:17 +0800

    Add files via upload

--- Task2.cpp
+++ Task2.cpp
@@ -9,6 +9,7 @@ int n[1000], m[1000], T=0, conv[1000]={0};
 int sum[1000]={0}, cnt=0;
 int diff[1000]={0}; 
 int mult[1000]={0};
+int Division[1000]={0};
 int empty[1000]={0};
 
 
@@ -18,7 +19,7 @@ class Number
 	void addition(int n[], int);
 	void difference(int, int n[], int, int, int m[], int);
 	void multiplication(int n[], int, int m[], int);
-	void division(int,int);
+	void division(int n[], int, int m[], int);
 	void interconvert(int n[], int, int); 
 	int base;
 
@@ -248,6 +249,41 @@ void Number::interconvert(int n[], int i, int base2)
 	
 }
 
+void Number::division(int n[], int i, int m[], int j)
+{
+	int z, n1=0, m1=0, answer=0;
+	for(z=0; z<i; z++)
+	{
+		n1 = n1 + pow(object.base,z) * n[z];
+	} 
+
+	for(z=0; z<i; z++)
+	{
+		m1 = m1 + pow(object.base,z) * m[z];
+	} 
+
+	if(m1==0)
+	{
+		cout << "Division by zero" << endl;
+		exit(0);
+	}
+
+
+	else
+	{
+		answer = n1/m1;
+		for(z=0; answer!=0; z++)
+		{
+			Division[z] = answer % (object.base);
+			answer = answer/(object.base);
+			T++;
+		} 	
+	}
+	
+
+
+}
+
 
 
 
@@ -263,6 +299,7 @@ int main()
 		for(int l=0; l<1000; l++)
 		{
 		
+			Division[l] = 0;			
 			sum[l] = 0;
 			diff[l] = 0;
 			mult[l] = 0;
@@ -275,7 +312,7 @@ int main()
 
 	cout << "Enter choice of operation: \n";
 	cout << "1. + for addition \n2. - for difference\n";
-	cout << "3. * for multiplication \n4. # for interconversion\n";
+	cout << "3. * for multiplication \n4. / for division \n5. # for interconversion\n";
 
 	cin >> ch;
 
@@ -747,6 +784,166 @@ int main()
 			cout << "PRODUCT: "<< ANS <<endl;
 			break;
 
+		case '/':
+			cout << "Enter A: ";
+			cin >> A;
+			len = strlen(A);
+			
+			for(i=0; i<len; i++)
+			{
+				switch(A[len-i-1]){
+					case '0': x[i] = 0;
+					break;
+					case '1': x[i] = 1;
+					break;
+					case '2': x[i] = 2;
+					break;
+					case '3': x[i] = 3;
+					break;
+					case '4': x[i] = 4;
+					break;
+					case '5': x[i] = 5;
+					break;
+					case '6': x[i] = 6;
+					break;
+					case '7': x[i] = 7;
+					break;
+					case '8': x[i] = 8;
+					break;
+					case '9': x[i] = 9;
+					break;
+					case 'A': x[i] = 10;
+					break;
+					case 'B': x[i] = 11;
+					break;
+					case 'C': x[i] = 12;
+					break;
+					case 'D': x[i] = 13;
+					break;
+					case 'E': x[i] = 14;
+					break;
+					case 'F': x[i] = 15;
+					break;
+					default : cout << "invalid input" << endl;
+							  goto label;
+					break;
+				}
+				cnt1++;
+			}
+
+			cout << "Enter B: ";
+			cin >> B;
+			len = strlen(B);
+			
+			for(j=0; j<len; j++)
+			{
+				switch(B[len-j-1]){
+					case '0': y[j] = 0;
+					break;
+					case '1': y[j] = 1;
+					break;
+					case '2': y[j] = 2;
+					break;
+					case '3': y[j] = 3;
+					break;
+					case '4': y[j] = 4;
+					break;
+					case '5': y[j] = 5;
+					break;
+					case '6': y[j] = 6;
+					break;
+					case '7': y[j] = 7;
+					break;
+					case '8': y[j] = 8;
+					break;
+					case '9': y[j] = 9;
+					break;
+					case 'A': y[j] = 10;
+					break;
+					case 'B': y[j] = 11;
+					break;
+					case 'C': y[j] = 12;
+					break;
+					case 'D': y[j] = 13;
+					break;
+					case 'E': y[j] = 14;
+					break;
+					case 'F': y[j] = 15;
+					break;
+					default : cout << "invalid input" << endl;
+							  goto label;
+					break;
+				}
+				cnt2++;
+			}
+
+			object.division(x, cnt1, y, cnt2);
+			
+			/* for(i=0; i<cnt; i++)
+			{
+				cout << Division[i] << " ";
+			} */
+			cout << endl;
+			
+
+			for(i=0; i<T; i++)
+			{
+				switch(Division[T-i-1]){
+					case 0: ANS[i] = '0';
+					break;
+					case 1: ANS[i] = '1';
+					break;
+					case 2: ANS[i] = '2';
+					break;
+					case 3: ANS[i] = '3';
+					break;
+					case 4: ANS[i] = '4';
+					break;
+					case 5: ANS[i] = '5';
+					break;
+					case 6: ANS[i] = '6';
+					break;
+					case 7: ANS[i] = '7';
+					break;
+					case 8: ANS[i] = '8';
+					break;
+					case 9: ANS[i] = '9';
+					break;
+					case 10: ANS[i] = 'A';
+					break;
+					case 11: ANS[i] = 'B';
+					break;
+					case 12: ANS[i] = 'C';
+					break;
+					case 13: ANS[i] = 'D';
+					break;
+					case 14: ANS[i] = 'E';
+					break;
+					case 15: ANS[i] = 'F';
+					break;
+				};
+			}
+			ANS[T]='\0';
+			i=0;
+			while(ANS[i]=='0')
+			{
+				i++;
+			}
+			f=0;
+			for(f=0; ANS[i]!='\0'; f++)
+			{
+				ANS[f] = ANS[i];
+				i++;
+			}
+			ANS[f]='\0';
+			cout << "QOUTIENT: "<< ANS <<endl;
+			break;
+
+
+			
+
+			
+
 		case '#':
 			cout << "Enter A: ";
 			cin >> A;
@@ -1016,6 +1213,58 @@ int main()
 			cout << "PRODUCT: " << ans <<endl;
 			ans =0;
 			break;
+
+		case '/':
+			cout << "Enter A: ";
+			cin >> a;
+			d = a;
+			
+			for(i=0; a!=0; i++)
+			{
+				x[i] = a%10;
+				if(x[i]>=object.base)
+				{
+					cout << "invalid input";
+					goto label;
+				}
+				a = a/10;
+				cnt1++;
+			}
+
+			cout << "Enter B: ";
+			cin >> b;
+			e = b;
+
+			for(j=0; b!=0; j++)
+			{
+				y[j] = b%10;
+				if(y[j]>=object.base)
+				{
+					cout << "invalid input";
+					goto label;
+				}
+				b = b/10;
+				cnt2++;
+			}
+
+			object.division(x, cnt1, y, cnt2);
+
+			/* for(i=0; i<cnt; i++)
+			{
+				cout << Division[i];
+			} */
+			cout << endl;
+
+			for(i=0; i<T; i++)
+			{
+				ans = ans + pow(10,i) * Division[i];
+			}
+
+			cout << "DIVISION: " << ans <<endl;
+			ans =0;
+			break;
+
+			
 
 		case '#':
 			cout << "Enter A: ";
